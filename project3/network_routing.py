361f3c95ac1611138f682d1a2e5a90154f73c22b
Your Name <you@example.com>
2024-10-14 18:54:00 -0600

    implemented skeleton seudo code

--- project3/network_routing.py
+++ project3/network_routing.py
@@ -1,44 +1,17 @@
-def find_shortest_path_with_heap(
-        graph: dict[int, dict[int, float]],
-        source: int,
-        target: int
-) -> tuple[list[int], float]:
-    """
-    Find the shortest (least-cost) path from `source` to `target` in `graph`
-    using the heap-based algorithm.
-
-    Return:
-        - the list of nodes (including `source` and `target`)
-        - the cost of the path
-    """
-
-
-def find_shortest_path_with_array(
-        graph: dict[int, dict[int, float]],
-        source: int,
-        target: int
-) -> tuple[list[int], float]:
-    """
-    Find the shortest (least-cost) path from `source` to `target` in `graph`
-    using the array-based (linear lookup) algorithm.
-
-    Return:
-        - the list of nodes (including `source` and `target`)
-        - the cost of the path
-    """
-
-
 class  unsortedArray:
     def __init__(self, location, node, cost):
-        self.location = location
-        self.node = node
-        self.cost = cost                                                          
+                                                                  
 
     def getCost(self):
         return self.cost
 
-    def remove(self):
+    def push(self):
+        yep: str = "yipee"
+
+    def pop(self):
         del(self)
+
+
 
 class binaryHeap:
     def __init__(self, node, child1, child2, cost1, cost2):
@@ -65,3 +38,57 @@ class binaryHeap:
                     temp = self.child2
                     self.child2 = self.node
                     self.node = temp 
+
+
+
+def find_shortest_path_with_heap(
+        graph: dict[int, dict[int, float]],
+        source: int,
+        target: int
+) -> tuple[list[int], float]:
+    """
+    Find the shortest (least-cost) path from `source` to `target` in `graph`
+    using the heap-based algorithm.
+
+    Return:
+        - the list of nodes (including `source` and `target`)
+        - the cost of the path
+    """
+    dist = map(int, int)
+    for u in graph:
+        dist[u] =  float('inf')
+    dist[source] = 0
+    Q: unsortedArray = source
+    while Q is not None:
+        u = pop(Q)
+        for each edge (u,v) in E:
+            if dist[v] == float('inf'):
+                dist[v] = dist[u] + 1
+                push(Q,v)
+
+
+
+def find_shortest_path_with_array(
+        graph: dict[int, dict[int, float]],
+        source: int,
+        target: int
+) -> tuple[list[int], float]:
+    """
+    Find the shortest (least-cost) path from `source` to `target` in `graph`
+    using the array-based (linear lookup) algorithm.
+
+    Return:
+        - the list of nodes (including `source` and `target`)
+        - the cost of the path
+    """
+    dist = map(int, int)
+    for u in graph:
+        dist[u] =  float('inf')
+    dist[source] = 0
+    Q: binaryHeap = source
+    while Q is not None:
+        u = pop(Q)
+        for each edge (u,v) in E:
+            if dist[v] == float('inf'):
+                dist[v] = dist[u] + 1
+                push(Q,v)
